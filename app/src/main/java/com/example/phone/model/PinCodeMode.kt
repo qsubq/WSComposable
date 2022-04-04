c17017832d6be383ff9c2de30bcdf70f57609b00
user4 <user1@user4.ru>
2022-04-04 11:45:33 +0300

    commit

--- app/src/main/java/com/example/phone/model/PinCodeMode.kt
+++ app/src/main/java/com/example/phone/model/PinCodeMode.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -18,24 +20,53 @@ import com.example.phone.common.theme.Main2
 
 @Composable
 fun PinCode(navController: NavController) {
-    Column(Modifier.fillMaxSize().background(brush = Brush.verticalGradient(
-        colors = listOf(
-            Main2,
-            Main1
-        )))) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Main2,
+                        Main1
+                    )
+                )
+            ), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "Введите этот пин-код на смартфоне",
             color = Color.White,
-            fontSize = 14.sp,
+            fontSize = 14.sp, textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold
         )
         var text by remember { mutableStateOf("") }
 
-        TextField(
-            value = text,
-            onValueChange = { text = it },
-            colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = LightBlue100)
-        )
+        Row(Modifier.fillMaxWidth()) {
+            TextField(
+                modifier = Modifier.size(20.dp),
+                value = text,
+                onValueChange = { text = it },
+                colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = LightBlue100)
+            )
+            TextField(
+                modifier = Modifier.size(20.dp),
+                value = text,
+                onValueChange = { text = it },
+                colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = LightBlue100)
+            )
+            TextField(
+                modifier = Modifier.size(20.dp),
+                value = text,
+                onValueChange = { text = it },
+                colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = LightBlue100)
+            )
+            TextField(
+                modifier = Modifier.size(20.dp),
+                value = text,
+                onValueChange = { text = it },
+                colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = LightBlue100)
+            )
+        }
+
         Button(
             onClick = { navController.navigate("verifyCh") },
             modifier = Modifier
